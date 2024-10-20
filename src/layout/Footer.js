6d672e030ae53134744bdc9e9ef20f2f65e4c917
CoderArpit5439 <officialarpit5439@gmail.com>
2024-10-20 18:56:03 +0530

    add footer, product list, single product, about, contact

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,10 +1,159 @@
 import React from 'react'
+import logo from '../assets/image/logo.png';
+
 
 const Footer = () => {
   return (
-    <div>
-        
-    </div>
+    <footer class="main">
+      <section class="newsletter mb-15">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="position-relative newsletter-inner">
+                <div class="newsletter-content">
+                  <h2 class="mb-20">
+                    Explore Our Latest <br></br>
+                    Sunglass Collections
+
+                  </h2>
+                  <p class="mb-45"> encourages visitors to browse through various sunglasses collections. From Classic Aviators to <span class="text-brand">Trendy Cat-Eyes</span></p>
+                </div>
+                <img src="assets/imgs/our/sunglass.png" alt="sunglass" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="featured section-padding">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-1-5 col-md-4 col-12 col-sm-6 mb-md-4 mb-xl-0">
+              <div class="banner-left-icon d-flex align-items-center wow fadeIn animated animated" style={{ visibility: "visible" }}>
+                <div class="banner-icon">
+                  <img src="assets/imgs/theme/icons/icon-1.svg" alt="" />
+                </div>
+                <div class="banner-text">
+                  <h3 class="icon-box-title">Best prices &amp; offers</h3>
+                  <p>Orders $50 or more</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-1-5 col-md-4 col-12 col-sm-6">
+              <div class="banner-left-icon d-flex align-items-center wow fadeIn animated animated" style={{ visibility: "visible" }}>
+                <div class="banner-icon">
+                  <img src="assets/imgs/theme/icons/icon-2.svg" alt="" />
+                </div>
+                <div class="banner-text">
+                  <h3 class="icon-box-title">Free delivery</h3>
+                  <p>24/7 amazing services</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-1-5 col-md-4 col-12 col-sm-6">
+              <div class="banner-left-icon d-flex align-items-center wow fadeIn animated animated" style={{ visibility: "visible" }}>
+                <div class="banner-icon">
+                  <img src="assets/imgs/theme/icons/icon-3.svg" alt="" />
+                </div>
+                <div class="banner-text">
+                  <h3 class="icon-box-title">Great daily deal</h3>
+                  <p>When your our customer</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-1-5 col-md-4 col-12 col-sm-6">
+              <div class="banner-left-icon d-flex align-items-center wow fadeIn animated animated" style={{ visibility: "visible" }}>
+                <div class="banner-icon">
+                  <img src="assets/imgs/theme/icons/icon-4.svg" alt="" />
+                </div>
+                <div class="banner-text">
+                  <h3 class="icon-box-title">Wide assortment</h3>
+                  <p>Mega Discounts</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-1-5 col-md-4 col-12 col-sm-6">
+              <div class="banner-left-icon d-flex align-items-center wow fadeIn animated animated" style={{ visibility: "visible" }}>
+                <div class="banner-icon">
+                  <img src="assets/imgs/theme/icons/icon-5.svg" alt="" />
+                </div>
+                <div class="banner-text">
+                  <h3 class="icon-box-title">Easy returns</h3>
+                  <p>Within 30 days</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-1-5 col-md-4 col-12 col-sm-6 d-xl-none">
+              <div class="banner-left-icon d-flex align-items-center wow fadeIn animated animated" style={{ visibility: "visible" }}>
+                <div class="banner-icon">
+                  <img src="assets/imgs/theme/icons/icon-6.svg" alt="" />
+                </div>
+                <div class="banner-text">
+                  <h3 class="icon-box-title">Safe delivery</h3>
+                  <p>Within 30 days</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="section-padding footer-mid">
+        <div class="container pt-15 pb-20">
+          <div class="row text-center">
+
+          <div class="col">
+              <div class="widget-about font-md mb-md-3 mb-lg-3 mb-xl-0">
+                <div class="logo mb-30">
+                  <a href="index.html" class="mb-15"><img src={logo} alt="logo" /></a>
+                  <p class="font-lg text-heading">Awesome Eyewear Store </p>
+                </div>
+               
+              </div>
+            </div>
+            <div class="footer-link-widget col">
+              <h4 class="widget-title">Company</h4>
+              <ul class="footer-list mb-sm-5 mb-md-0">
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Delivery Information</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">Terms &amp; Conditions</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">Support Center</a></li>
+              </ul>
+            </div>
+            <div class="footer-link-widget col">
+              <h4 class="widget-title">Popular</h4>
+              <ul class="footer-list mb-sm-5 mb-md-0">
+                <li><a href="#">Cat Eye &amp; Squre Frame</a></li>
+                <li><a href="#">Full Frame</a></li>
+                <li><a href="#">Half Rim Frame</a></li>
+                <li><a href="#">Rimless Frame</a></li>
+                <li><a href="#">Men's Frame</a></li>
+                <li><a href="#">Women's Frame</a></li>
+                <li><a href="#">Kid's Frame</a></li>
+              </ul>
+            </div>
+            <div class="footer-link-widget col">
+              <h4 class="widget-title">Contact</h4>
+              <ul class="contact-infor">
+                  <li><img src="assets/imgs/theme/icons/icon-location.svg" alt="" /><strong>Address: </strong> <span>Sharma Market, Vill. Mundla Dostdar, Kampel Road, Indore, MP</span></li>
+                  <li><img src="assets/imgs/theme/icons/icon-contact.svg" alt="" /><strong>Call Us:</strong><span>(+91) - 93000 62750</span></li>
+                  <li><img src="assets/imgs/theme/icons/icon-email-2.svg" alt="" /><strong>Email:</strong><span>shreenetramoptical@gmail.com</span></li>
+                  <li><img src="assets/imgs/theme/icons/icon-clock.svg" alt="" /><strong>Hours:</strong><span>10:00 AM - 06:00 PM, Mon - Sat</span></li>
+                </ul>
+                <div class="mobile-social-icon" style={{marginTop:"30px",justifyContent:"center"}}>
+              <h6>Follow Us</h6>
+              <a href="#"><img src="assets/imgs/theme/icons/icon-facebook-white.svg" alt="" /></a>
+              <a href="#"><img src="assets/imgs/theme/icons/icon-twitter-white.svg" alt="" /></a>
+              <a href="#"><img src="assets/imgs/theme/icons/icon-instagram-white.svg" alt="" /></a>
+              <a href="#"><img src="assets/imgs/theme/icons/icon-pinterest-white.svg" alt="" /></a>
+              <a href="#"><img src="assets/imgs/theme/icons/icon-youtube-white.svg" alt="" /></a>
+            </div>
+            <p class="font-sm">Up to 15% discount on your first subscribe</p>
+          </div>
+            </div>
+          </div>
+      </section>
+    </footer>
   )
 }
 
---
+++ src/pages/Aboutus.jsx
@@ -0,0 +1,102 @@
+import React from 'react'
+
+const Aboutus = () => {
+    return (
+        <div class="page-content pt-50">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-10 col-lg-12 m-auto">
+                        <section class="row align-items-center mb-50">
+                            <div class="col-lg-6">
+                                <img src="assets/imgs/our/shop-image.jpg" alt="" class="border-radius-15 mb-md-3 mb-lg-0 mb-sm-4"/>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="pl-25">
+                                    <h2 class="mb-30">Welcome to Shree Netram</h2>
+                                    <p class="mb-25">Established in 2023, Shree Netram Optical has become a premier eyewear brand in Indore. Our success stems from prioritizing customer satisfaction. By offering unbeatable deals and exceptional service, we've earned the trust of our customers. At Shree Netram Optical, customer happiness is our top priority, driving our journey towards excellence.</p>
+                                  </div>
+                            </div>
+                        </section>
+                        <section class="text-center mb-50">
+                            <h2 class="title style-3 mb-40">What We Provide?</h2>
+                            <div class="row">
+                                <div class="col-lg-4 col-md-6 mb-24">
+                                    <div class="featured-card">
+                                        <img src="assets/imgs/theme/icons/icon-1.svg" alt=""/>
+                                            <h4>Our Vision</h4>
+                                            <p>We're committed to improving your vision and style. Our team uses advanced technology and personalized service to ensure you get the perfect glasses. See better, look better with us!</p>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-6 mb-24">
+                                    <div class="featured-card">
+                                        <img src="assets/imgs/theme/icons/icon-2.svg" alt=""/>
+                                            <h4>Our Products</h4>
+                                            <p>Discover our wide range of glasses and sunglasses. Find your perfect pair for every occasion and style. Explore now for the best selection!</p>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-6 mb-24">
+                                    <div class="featured-card">
+                                        <img src="assets/imgs/theme/icons/icon-3.svg" alt=""/>
+                                            <h4>Free Delivery</h4>
+                                            <p>Enjoy hassle-free shopping with complimentary standard shipping on all orders over 999rs. We strive to deliver your glasses quickly and efficiently.</p>
+                                       
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-6 mb-24">
+                                    <div class="featured-card">
+                                        <img src="assets/imgs/theme/icons/icon-4.svg" alt=""/>
+                                            <h4>Easy Returns</h4>
+                                            <p>Our 30-day return policy allows you to try your glasses risk-free. Simply initiate a return within 30 days for a full refund or exchange. Contact us for a prepaid return label.</p>
+                                       
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-6 mb-24">
+                                    <div class="featured-card">
+                                        <img src="assets/imgs/theme/icons/icon-5.svg" alt=""/>
+                                            <h4>100% Satisfaction</h4>
+                                            <p>We stand behind the quality of our glasses. If you're not completely satisfied, let us know within 30 days for a full refund or exchange. Your satisfaction is our top priority.</p>
+                                       
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-6 mb-24">
+                                    <div class="featured-card">
+                                        <img src="assets/imgs/theme/icons/icon-6.svg" alt=""/>
+                                            <h4>Our Services</h4>
+                                            <p>Experience our exceptional eye care services. From comprehensive eye exams to specialized treatments, we're here to ensure your vision health is in good hands.</p>
+                                       
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
+            <section class="container mb-50 d-none d-md-block">
+                <div class="row about-count">
+                    <div class="col-lg-1-5 col-md-6 text-center mb-lg-0 mb-md-5">
+                        <h1 class="heading-1"><span class="count">1</span>+</h1>
+                        <h4>Business years</h4>
+                    </div>
+                    <div class="col-lg-1-5 col-md-6 text-center">
+                        <h1 class="heading-1"><span class="count">200</span>+</h1>
+                        <h4>Happy clients</h4>
+                    </div>
+                    <div class="col-lg-1-5 col-md-6 text-center">
+                        <h1 class="heading-1"><span class="count">4</span>+</h1>
+                        <h4>Connected Hospital</h4>
+                    </div>
+                    <div class="col-lg-1-5 col-md-6 text-center">
+                        <h1 class="heading-1"><span class="count">1</span></h1>
+                        <h4>Our Branch</h4>
+                    </div>
+                    <div class="col-lg-1-5 col-md-6 text-center">
+                        <h1 class="heading-1"><span class="count">8</span>+</h1>
+                        <h4>Team Member</h4>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Aboutus
